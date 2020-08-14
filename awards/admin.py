05cab634d992c5d259bb4eee6d80d88cd53bd55c
Fred Ojure <fredojure@hotmail.com>
2020-08-14 18:41:49 +0300

    Performed a migration after adding a few fields

--- awards/admin.py
+++ awards/admin.py
@@ -2,6 +2,9 @@ from django.contrib import admin
 from .models import Developer, Submission, Votes, tags
 
 # Register your models here.
+class SubmissionAdmin(admin.ModelAdmin):
+    filter_horizontal = ('tags',)
+    
 admin.site.register(Developer)
 admin.site.register(Submission)
 admin.site.register(Votes)
