8093db4b91fb7af03905bdca2447d61a9f316393
Fred Ojure <fredojure@hotmail.com>
2020-08-14 17:08:35 +0300

    Did a migrate for Submissions and Votes models

--- awards/models.py
+++ awards/models.py
@@ -21,6 +21,17 @@ class tags(models.Model):
 class Submission(models.Model):
     title = models.CharField(max_length=60)
     description = models.TextField()
-    developer = models.ForeignKey(Developer)
+    developer = models.ForeignKey(Developer, on_delete=models.DO_NOTHING)
+    url_link = models.CharField(max_length=40)
+    date_judged = models.DateTimeField()
     tags = models.ManyToManyField(tags)
     submission_date = models.DateTimeField(auto_now_add=True)
+
+class Votes(models.Model):
+    design = models.IntegerField()
+    usability = models.IntegerField()
+    creativity = models.IntegerField()
+    content = models.IntegerField()
+    mobile = models.IntegerField()
+    submission = models.ForeignKey(Submission, on_delete= models.DO_NOTHING)
+    developer = models.ForeignKey(Developer, on_delete=models.DO_NOTHING)
