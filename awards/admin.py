9f72f0696d1095398b9b56495b2a1d067a44fce7
Fred Ojure <fredojure@hotmail.com>
2020-08-14 18:23:33 +0300

    Created superuser and added models to admin page

--- awards/admin.py
+++ awards/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Developer, Submission, Votes, tags
 
 # Register your models here.
+admin.site.register(Developer)
+admin.site.register(Submission)
+admin.site.register(Votes)
+admin.site.register(tags)
