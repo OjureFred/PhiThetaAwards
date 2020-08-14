26d7b10cc2a0659adf021206e0990180636aaa75
Fred Ojure <fredojure@hotmail.com>
2020-08-15 01:41:33 +0300

    Added image upload process for Submissions

--- awards/urls.py
+++ awards/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.conf.urls import url
 from . import views
 
@@ -5,3 +7,5 @@ urlpatterns = [
     url('^$', views.welcome, name='welcome'),
     url(r'^search/', views.search_results, name='search_results')
 ]
+if settings.DEBUG:
+    urlpatterns+=static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
