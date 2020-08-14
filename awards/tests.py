53a83ea7ee71086714ab6a5944125620aa1345ee
Fred Ojure <fredojure@hotmail.com>
2020-08-14 18:12:38 +0300

    Wrote a unit test for Developer instance

--- awards/tests.py
+++ awards/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from .models import Developer, Submission, Votes, tags
 
 # Create your tests here.
+class DeveloperTestClass(TestCase):
+
+    #Set up method
+    def setUp(self):
+        self.fred = Developer(first_name='Fredrick', last_name='Ojure', email='fredojure@hotmail.com')
+    
+    #Test instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.fred, Developer))
