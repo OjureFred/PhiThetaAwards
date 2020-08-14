2638b564cb8b083fbfc3c6cb81f803db249b1463
Fred Ojure <fredojure@hotmail.com>
2020-08-15 00:48:20 +0300

    Added today-submission and past-submission proces/code

--- awards/views.py
+++ awards/views.py
@@ -1,6 +1,29 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import datetime as dt
 
 # Create your views here.
 def welcome(request):
     return render(request, 'welcome.html')
+
+def submissions_today(request):
+    date = dt.date.today()
+    submissions = Submission.todays_submission()
+    context = {"date": date, "news": news}
+    return render(request, 'all-awards/today_submission.html', context)
+
+def past_days_submission(request, past_date):
+    try:
+        #Converrts data from the string url
+        date = dt.datetime.strptime(past_date, '%Y-%m-%d').date()
+    except ValueError:
+        #Raise 404 error when ValueError is thrown
+        raise Http404()
+        assert False
+    
+    if date == dt.date.today():
+        return redirect(submissions_today)
+
+    submissions = Submission.days_submission(date)
+    context = {"date": date, "submissions": submissions}
+    return render(request, 'all-awards/past-submissions.html', context)
