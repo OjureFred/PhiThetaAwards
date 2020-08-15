238122212d0e68d9a73eb3237dc633954c87897b
Fred Ojure <fredojure@hotmail.com>
2020-08-15 16:06:04 +0300

    Made adjustments to submissions displayed on main page

--- awards/models.py
+++ awards/models.py
@@ -9,7 +9,7 @@ class Developer(models.Model):
     phone_number = models.CharField(max_length=10, blank=True)
 
     def __str__(self):
-        return self.first_name
+        return self.first_name + ' ' + self.last_name
     
     def save_developer(self):
         self.save()
